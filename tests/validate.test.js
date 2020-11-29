5f08b3519a9166cc67f280c5d1f167f14cebe519
pmw57 <paul.m.wilkins@gmail.com>
2020-11-29 17:14:52 +1300

    Group together terms tests

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -55,53 +55,50 @@ describe("When form is reset, reset input classes", function () {
         });
     });
     describe("terms", function () {
-        const $termsFormgroup = $termcheck.parentsUntil(".form-group");
-        const $termsFeedback = $termsFormgroup.find(".feedback");
-        const $termsStarrq = $termsFormgroup.find(".starrq");
-        it("removes glyphicon from terms", function () {
-            $termsFeedback.addClass("glyphicon");
-            $(".btn2").trigger("click");
-            expect($termsFeedback.attr("class")).to.not.contain("glyphicon");
+        const $termsFormgroup = $("#terms").parentsUntil(".form-group");
+        describe("terms feedback", function () {
+            const $termsFeedback = $termsFormgroup.find(".feedback");
+            it("removes glyphicon from terms", function () {
+                $termsFeedback.addClass("glyphicon");
+                $(".btn2").trigger("click");
+                expect($termsFeedback.attr("class")).to.not.contain("glyphicon");
+            });
+            it("removes glyphicon-ok from terms", function () {
+                $termsFeedback.addClass("glyphicon-ok");
+                $(".btn2").trigger("click");
+                expect($termsFeedback.attr("class")).to.not.contain("glyphicon-ok");
+            });
+            it("removes ok from terms", function () {
+                $termsFeedback.addClass("ok");
+                $(".btn2").trigger("click");
+                expect($termsFeedback.attr("class")).to.not.contain("ok");
+            });
         });
-        it("removes glyphicon-ok from terms", function () {
-            $termsFeedback.addClass("glyphicon-ok");
-            $(".btn2").trigger("click");
-            expect($termsFeedback.attr("class")).to.not.contain("glyphicon-ok");
+        describe("terms checkbox", function () {
+            const $termcheck = $("#termcheck");
+            it("adds checkbox ok to termcheck", function () {
+                $termcheck.removeClass("ok");
+                $(".btn2").trigger("click");
+                expect($termcheck.attr("class")).to.contain("ok");
+            });
+            it("removes checkbox warning from termcheck", function () {
+                $termcheck.addClass("warning");
+                $(".btn2").trigger("click");
+                expect($termcheck.attr("class")).to.not.contain("warning");
+            });
         });
-        it("removes ok from terms", function () {
-            $termsFeedback.addClass("ok");
-            $(".btn2").trigger("click");
-            expect($termsFeedback.attr("class")).to.not.contain("ok");
-        });
-        it("removes warning from starrq", function () {
-            $termsStarrq.addClass("warning");
-            $(".btn2").trigger("click");
-            expect($termsFeedback.attr("class")).to.not.contain("warning");
-        });
-        it("adds ok to starrq", function () {
-            $termsStarrq.removeClass("ok");
-            $(".btn2").trigger("click");
-            expect($termsStarrq.attr("class")).to.contain("ok");
-        });
-        it("adds checkbox ok to termcheck", function () {
-            $termcheck.removeClass("ok");
-            $(".btn2").trigger("click");
-            expect($termcheck.attr("class")).to.contain("ok");
-        });
-        it("removes checkbox warning from termcheck", function () {
-            $termcheck.addClass("warning");
-            $(".btn2").trigger("click");
-            expect($termcheck.attr("class")).to.not.contain("warning");
-        });
-        it("adds required ok to termsRequired", function () {
-            $termsRequired.removeClass("ok");
-            $(".btn2").trigger("click");
-            expect($termsRequired.attr("class")).to.contain("ok");
-        });
-        it("removes required warning from termsRequired", function () {
-            $termsRequired.addClass("warning");
-            $(".btn2").trigger("click");
-            expect($termsRequired.attr("class")).to.not.contain("warning");
+        describe("terms required", function () {
+            const $termsRequired = $("#termsRequired");
+            it("adds required ok to termsRequired", function () {
+                $termsRequired.removeClass("ok");
+                $(".btn2").trigger("click");
+                expect($termsRequired.attr("class")).to.contain("ok");
+            });
+            it("removes required warning from termsRequired", function () {
+                $termsRequired.addClass("warning");
+                $(".btn2").trigger("click");
+                expect($termsRequired.attr("class")).to.not.contain("warning");
+            });
         });
     });
 });
