fd20c774df7ffee5bd51575516b5961a931e1fbc
pmw57 <paul.m.wilkins@gmail.com>
2020-11-29 22:33:14 +1300

    Organise and improve the tests

--- tests/change-password.test.js
+++ tests/change-password.test.js
@@ -1,46 +1,52 @@
-describe("When reset password form is reset, reset input classes", function () {
-    const $inputGroup = $(".inputboxmodal2");
-    const $emailInput = $inputGroup.find(".input-check");
-    const $emailError = $inputGroup.find(".error");
-    const $emailFeedback = $inputGroup.find(".feedback");
-    it("Resets the error message", function () {
-        $emailError.html("test content");
-        $("#changepw [type=reset]").trigger("click");
-        expect($emailError.html()).to.equal("Your E-mail");
+describe("When change-password form is reset, update input messages", function () {
+    const passwordResetHandler = changePassword.eventHandler.passwordReset;
+    const fakeEvt = {};
+    describe("email error", function () {
+        const $emailGroup = $("#changepw .form-group").eq(1);
+        const $emailError = $emailGroup.find(".error");
+        it("Resets the error message", function () {
+            $emailError.html("test content");
+            passwordResetHandler(fakeEvt);
+            expect($emailError.html()).to.equal("Your E-mail");
+        });
+        it("Resets the error color", function () {
+            $emailError.removeClass("green");
+            passwordResetHandler(fakeEvt);
+            expect($emailError.attr("class")).to.not.contain("green");
+        });
+        it("Removes the error warning class", function () {
+            $emailError.addClass("warning");
+            passwordResetHandler(fakeEvt);
+            expect($emailError.attr("class")).to.not.contain("warning");
+        });
+        it("Adds the error ok class", function () {
+            $emailError.removeClass("ok");
+            passwordResetHandler(fakeEvt);
+            expect($emailError.attr("class")).to.contain("ok");
+        });
     });
-    it("Resets the error color", function () {
-        $emailError.removeClass("green");
-        $("#changepw [type=reset]").trigger("click");
-        expect($emailError.attr("class")).to.not.contain("green");
-    });
-    it("Removes the error warning class", function () {
-        $emailError.addClass("warning");
-        $("#changepw [type=reset]").trigger("click");
-        expect($emailError.attr("class")).to.not.contain("warning");
-    });
-    it("Adds the error ok class", function () {
-        $emailError.removeClass("ok");
-        $("#changepw [type=reset]").trigger("click");
-        expect($emailError.attr("class")).to.contain("ok");
-    });
-    it("Removes the feedback glyphicon class", function () {
-        $emailFeedback.addClass("glyphicon");
-        $("#changepw [type=reset]").trigger("click");
-        expect($emailFeedback.attr("class")).to.not.contain("glyphicon");
-    });
-    it("Removes the feedback glyphicon-ok class", function () {
-        $emailFeedback.addClass("glyphicon-ok");
-        $("#changepw [type=reset]").trigger("click");
-        expect($emailFeedback.attr("class")).to.not.contain("glyphicon-ok");
-    });
-    it("Removes the feedback ok class", function () {
-        $emailFeedback.addClass("ok");
-        $("#changepw [type=reset]").trigger("click");
-        expect($emailFeedback.attr("class")).to.not.contain("ok");
-    });
-    it("Removes the feedback glyphicon-remove class", function () {
-        $emailFeedback.addClass("glyphicon-remove");
-        $("#changepw [type=reset]").trigger("click");
-        expect($emailFeedback.attr("class")).to.not.contain("glyphicon-remove");
+    describe("email feedback", function () {
+        const $emailGroup = $("#changepw .form-group").eq(1);
+        const $emailFeedback = $emailGroup.find(".feedback");
+        it("Removes the feedback glyphicon class", function () {
+            $emailFeedback.addClass("glyphicon");
+            passwordResetHandler(fakeEvt);
+            expect($emailFeedback.attr("class")).to.not.contain("glyphicon");
+        });
+        it("Removes the feedback glyphicon-ok class", function () {
+            $emailFeedback.addClass("glyphicon-ok");
+            passwordResetHandler(fakeEvt);
+            expect($emailFeedback.attr("class")).to.not.contain("glyphicon-ok");
+        });
+        it("Removes the feedback ok class", function () {
+            $emailFeedback.addClass("ok");
+            passwordResetHandler(fakeEvt);
+            expect($emailFeedback.attr("class")).to.not.contain("ok");
+        });
+        it("Removes the feedback glyphicon-remove class", function () {
+            $emailFeedback.addClass("glyphicon-remove");
+            passwordResetHandler(fakeEvt);
+            expect($emailFeedback.attr("class")).to.not.contain("glyphicon-remove");
+        });
     });
 });
--- tests/validate.test.js
+++ tests/validate.test.js
@@ -1,61 +1,63 @@
-describe("When form is reset, reset input classes", function () {
-    const $firstnameGroup = $(".form-group").first();
+describe("When form is reset, reset input messages", function () {
     const registrationResetHandler = validate.eventHandler.registrationReset;
     const fakeEvt = {
         preventDefault: function fakeFunc() {}
     };
-    describe("firstname error", function () {
-        const $firstnameInput = $firstnameGroup.find(".check");
-        const $firstnameError = $firstnameGroup.find(".error");
-        it("updates the error name", function () {
-            $firstnameError.html("");
-            $(".btn2").trigger("click");
-            expect($firstnameError.html()).to.equal($firstnameInput.attr("name"));
+    describe("firstname", function () {
+        const $firstnameGroup = $("#registration .form-group").first();
+        describe("firstname error", function () {
+            const $firstnameError = $firstnameGroup.find(".error");
+            const firstnameName = $firstnameGroup.find("input").attr("name");
+            it("updates the error name", function () {
+                $firstnameError.html("");
+                $(".btn2").trigger("click");
+                expect($firstnameError.html()).to.equal(firstnameName);
+            });
+            it("removes warning from error", function () {
+                $firstnameError.addClass("warning");
+                $(".btn2").trigger("click");
+                expect($firstnameError.attr("class")).to.not.contain("warning");
+            });
+            it("adds ok to error", function () {
+                $firstnameError.removeClass("ok");
+                $(".btn2").trigger("click");
+                expect($firstnameError.attr("class")).to.contain("ok");
+            });
         });
-        it("removes warning from error", function () {
-            $firstnameError.addClass("warning");
-            $(".btn2").trigger("click");
-            expect($firstnameError.attr("class")).to.not.contain("warning");
+        describe("firstname feedback", function () {
+            const $firstnameFeedback = $firstnameGroup.find(".feedback");
+            it("removes glyphicon from feedback", function () {
+                $firstnameFeedback.addClass("glyphicon");
+                $(".btn2").trigger("click");
+                expect($firstnameFeedback.attr("class")).to.not.contain("glyphicon");
+            });
+            it("removes glyphicon-ok from feedback", function () {
+                $firstnameFeedback.addClass("glyphicon-ok");
+                $(".btn2").trigger("click");
+                expect($firstnameFeedback.attr("class")).to.not.contain("glyphicon-ok");
+            });
+            it("removes warning from feedback", function () {
+                $firstnameFeedback.addClass("warning");
+                $(".btn2").trigger("click");
+                expect($firstnameFeedback.attr("class")).to.not.contain("warning");
+            });
         });
-        it("adds ok to error", function () {
-            $firstnameError.removeClass("ok");
-            $(".btn2").trigger("click");
-            expect($firstnameError.attr("class")).to.contain("ok");
-        });
-    });
-    describe("firstname feedback", function () {
-        const $firstnameFeedback = $firstnameGroup.find(".feedback");
-        it("removes glyphicon from feedback", function () {
-            $firstnameFeedback.addClass("glyphicon");
-            $(".btn2").trigger("click");
-            expect($firstnameFeedback.attr("class")).to.not.contain("glyphicon");
-        });
-        it("removes glyphicon-ok from feedback", function () {
-            $firstnameFeedback.addClass("glyphicon-ok");
-            $(".btn2").trigger("click");
-            expect($firstnameFeedback.attr("class")).to.not.contain("glyphicon-ok");
-        });
-        it("removes warning from feedback", function () {
-            $firstnameFeedback.addClass("warning");
-            $(".btn2").trigger("click");
-            expect($firstnameFeedback.attr("class")).to.not.contain("warning");
-        });
-    });
-    describe("firstname required", function () {
-        const $firstnameRequired = $firstnameGroup.find(".starrq");
-        it("removes warning from starrq", function () {
-            $firstnameRequired.addClass("warning");
-            $(".btn2").trigger("click");
-            expect($firstnameRequired.attr("class")).to.not.contain("warning");
-        });
-        it("adds ok to starrq", function () {
-            $firstnameRequired.removeClass("ok");
-            $(".btn2").trigger("click");
-            expect($firstnameRequired.attr("class")).to.contain("ok");
+        describe("firstname required", function () {
+            const $firstnameRequired = $firstnameGroup.find(".starrq");
+            it("removes warning from starrq", function () {
+                $firstnameRequired.addClass("warning");
+                $(".btn2").trigger("click");
+                expect($firstnameRequired.attr("class")).to.not.contain("warning");
+            });
+            it("adds ok to starrq", function () {
+                $firstnameRequired.removeClass("ok");
+                $(".btn2").trigger("click");
+                expect($firstnameRequired.attr("class")).to.contain("ok");
+            });
         });
     });
     describe("terms", function () {
-        const $termsFormgroup = $("#terms").parentsUntil(".form-group");
+        const $termsFormgroup = $("#terms").closest(".form-group");
         describe("terms feedback", function () {
             const $termsFeedback = $termsFormgroup.find(".feedback");
             it("removes glyphicon from terms", function () {
@@ -75,7 +77,7 @@ describe("When form is reset, reset input classes", function () {
             });
         });
         describe("terms checkbox", function () {
-            const $termcheck = $("#termcheck");
+            const $termcheck = $termsFormgroup.find("#termcheck");
             it("adds checkbox ok to termcheck", function () {
                 $termcheck.removeClass("ok");
                 $(".btn2").trigger("click");
@@ -88,7 +90,7 @@ describe("When form is reset, reset input classes", function () {
             });
         });
         describe("terms required", function () {
-            const $termsRequired = $("#termsRequired");
+            const $termsRequired = $termsFormgroup.find("#termsRequired");
             it("adds required ok to termsRequired", function () {
                 $termsRequired.removeClass("ok");
                 $(".btn2").trigger("click");
