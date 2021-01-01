9b98edf512e40ecf3c0276972fbef011c5c9a598
pmw57 <paul.m.wilkins@gmail.com>
2021-01-01 21:54:55 +1300

    Tidy up the login code by making the comparison clearer, and placing reset code above submit code.

--- registration3/login.js
+++ registration3/login.js
@@ -2,26 +2,23 @@ const login = (function makeLogin() {
     function loginInputHandler(evt) {
         validate.check(evt.target);
     }
-
-    function loginSubmitHandler(evt) {
-        validate.checkFormEmpty("#login");
-        if ($("#login .warning").length !== 0) {
-            evt.preventDefault();
-        }
-    }
-
     function loginResetHandler() {
         inputStatus.resetForm($("#login"), validate.fn.getName);
     }
-
+    function loginSubmitHandler(evt) {
+        validate.checkFormEmpty("#login");
+        if ($("#login .warning").length > 0) {
+            evt.preventDefault();
+        }
+    }
     $("#login .form-group").on("focusin focusout input", loginInputHandler);
-    $("#login").on("submit", loginSubmitHandler);
     $("#login").on("reset", loginResetHandler);
+    $("#login").on("submit", loginSubmitHandler);
     return {
         eventHandler: {
             loginInput: loginInputHandler,
-            loginSubmit: loginSubmitHandler,
-            loginReset: loginResetHandler
+            loginReset: loginResetHandler,
+            loginSubmit: loginSubmitHandler
         }
     };
 }());
